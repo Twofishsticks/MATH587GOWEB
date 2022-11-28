befa1594da38c2f6a6ab6d4825a8a568fb4169c8
Trey Sturman <tssturman@gmail.com>
2022-11-28 10:27:23 -0500

    AKS alg using pascal's triangle (thanks csce 350)

--- handlers.article.go
+++ handlers.article.go
@@ -20,7 +20,7 @@ func showIndexPage(c *gin.Context) {
 // If the header doesn't specify this, HTML is rendered, provided that
 // the template name is present
 func render(c *gin.Context, data gin.H, templateName string) {
-
+	// for database usage if needed
 	switch c.Request.Header.Get("Accept") {
 	case "application/json":
 		// Respond with JSON
@@ -33,4 +33,33 @@ func render(c *gin.Context, data gin.H, templateName string) {
 		c.HTML(http.StatusOK, templateName, data)
 	}
 
+}
+
+// prime if ( x – 1 )^n – ( x^n – 1) is divisible by n, x being some value, n being input value
+// true if prime, false if nonprime
+func AKSalgProcess(n int) bool {
+	coef := allCoef(n)
+	// now check to see if ALL coef are divisable by n
+	i := n
+	for (i > 0) && (coef[i]%n == 0) { // go until there's no more coef to check or coef is not divisable
+		i-- // check the next coef value
+	}
+
+	return i < 0 // if loop broke on non divisable, return false
+}
+
+// returns all coefficients of ( x – 1 )^n – ( x^n – 1), using pascal's triangle
+func allCoef(n int) []int {
+	var coef []int
+	for i := 0; i < n; i++ { // for each ^n
+		coef[0] = -coef[0] // swap the first val
+		coef[1+i] = 1      // set pascal triangle position
+
+		for j := i; j > 0; j-- {
+			coef[j] = coef[j-1] - coef[j]
+		}
+	}
+	coef[0]++ // the "-(x^n -1)" one
+	coef[n]-- // the (x-1)^n one
+	return coef
 }
