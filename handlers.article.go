2a273272e89a213906ea17252df07022580992f5
Trey Sturman <tssturman@gmail.com>
2022-12-04 09:10:16 -0500

    functional aks code, included exe just in case

--- handlers.article.go
+++ handlers.article.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"strconv"
 
+	//"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,8 +22,14 @@ func showIndexPage(c *gin.Context) {
 func showAnswer(c *gin.Context) {
 	numString := c.PostForm("number")
 	print(numString)
+	n, err := strconv.Atoi(numString)
+	if err != nil {
+		// issue w string -> int
+		panic(err)
+	}
+	//println(n)
 
-	if AKSalgProcess(numString) { // if prime
+	if aks(n) { // if prime
 		render(c, gin.H{
 			"title": "Prime Found"}, "index.html")
 		print("Prime")
@@ -34,6 +42,7 @@ func showAnswer(c *gin.Context) {
 		//"ErrorMessage": "Invalid credentials provided"})
 		print("Nonprime")
 	}
+
 }
 
 // Render one of HTML, JSON or CSV based on the 'Accept' header of the request
@@ -55,6 +64,7 @@ func render(c *gin.Context, data gin.H, templateName string) {
 
 }
 
+/*
 // prime if ( x – 1 )^n – ( x^n – 1) is divisible by n, x being some value, n being input value
 // true if prime, false if nonprime
 func AKSalgProcess(num string) bool {
@@ -88,4 +98,31 @@ func allCoef(n int) []int {
 	coef[0]++ // the "-(x^n -1)" one
 	coef[n]-- // the (x-1)^n one
 	return coef
+}
+*/
+
+func bc(p int) []int64 {
+	c := make([]int64, p+1)
+	r := int64(1)
+	for i, half := 0, p/2; i <= half; i++ {
+		c[i] = r
+		c[p-i] = r
+		r = r * int64(p-i) / int64(i+1)
+	}
+	for i := p - 1; i >= 0; i -= 2 {
+		c[i] = -c[i]
+	}
+	return c
+}
+
+func aks(p int) bool {
+	c := bc(p)
+	c[p]-- // for the (x^p-1)
+	c[0]++ // for the (-1)^p
+	for _, d := range c {
+		if d%int64(p) != 0 { // making sure it's all divisable by num
+			return false
+		}
+	}
+	return true
 }
